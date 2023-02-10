7cb4f955f8583a6210556bb234c987559eeeb2dc
JLerry05 <jlerry005@gmail.com>
2023-02-10 23:45:56 +0800

    02-09-23

---
+++ src/components/contacts.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const contact = ({ img, name, phone, email }) => {
+    return (
+
+        <div className="contacts">
+
+            <div className="contact-card">
+                <img src={img} />
+                <h3>{name}</h3>
+                <div className="info-group">
+                    <img src="./images/phone-icon.png" />
+                    <p>{phone}</p>
+                </div>
+                <div className="info-group">
+                    <img src="./images/mail-icon.png" />
+                    <p>{email}</p>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default contact;
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import PhotoGrid from '../assets/photo-grid.svg'
 
 const Hero = () => {
+    // const date = new Date()
+    // const hours = date.getHours() % 24;
+    // let timeOfDay;
+
+    // if (hours < 12) {
+    //     timeOfDay = 'Morning'
+    // } else if (hours >= 12 && hours < 17) {
+    //     timeOfDay = 'Afternoon'
+    // } else {
+    //     timeOfDay = 'Evening'
+    // };
+
     return (
         <section className='hero--container'>
             <img className='hero--photo' src={PhotoGrid} alt="" />
             <h1 className='hero--header'>Online experiences</h1>
             <p className='hero--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            {/* <h1>Good {timeOfDay}!</h1> */}
         </section>
+
+
     )
 }
 

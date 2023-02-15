c5ae0d8ead1b946b41cc7cadf526762b87621cf6
JLerry05 <jlerry005@gmail.com>
2023-02-16 00:13:54 +0800

    02-16-23

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import './App.css';
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import Card from './components/card'
+import data from './components/data'
 
 import Photo from './assets/katie-zaferes.png';
 
 function App() {
+
+  const card = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        review={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
+
   return (
     <div className="app--container">
       <Navbar />
-      {/* <Hero /> */}
-      <Card
-        img={Photo}
-        rating="5.0"
-        review={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      <Hero />
+      <section className='card--container'>
+        {card}
+      </section>
+
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,14 +5,13 @@ import Star from '../assets/star.png'
 const card = (props) => {
     console.log(props)
     return (
-
         <div className='card'>
             <img src={props.img} alt="card-img" className='card--image' />
             <div className='card--stats'>
                 <img className='card--star' src={Star} alt="star" />
                 <span>{props.rating}</span>
-                <span className='gray'>{props.review} • </span>
-                <span className='gray'>{props.country}</span>
+                <span className='gray'>({props.review}) • </span>
+                <span className='gray'>{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className='bold'>From ${props.price}</span> / person</p>
